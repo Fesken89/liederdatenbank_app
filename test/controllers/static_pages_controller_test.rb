7fac60be8f17e3ae723afa11bd0d01a678a67318
flo <flo@debian.icef.local>
2015-05-04 17:07:23 +0200

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Liederdatenbank des Heilsbrunnen"
+    assert_select "title", "Liederdatenbank des Heilsbrunnen"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Liederdatenbank des Heilsbrunnen"
+    assert_select "title", "Liederdatenbank des Heilsbrunnen"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Liederdatenbank des Heilsbrunnen"
+    assert_select "title", "Liederdatenbank des Heilsbrunnen"
   end
 
 end
